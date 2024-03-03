41b4c0f5041e67f750e53e4b22fe3f153a110023
王烜烨 <kevin911@169-231-209-21.wireless.ucsb.edu>
2024-03-02 22:09:47 -0800

    customer->business info page

--- src/BusinessInfo.js
+++ src/BusinessInfo.js
@@ -23,8 +23,17 @@ function BusinessInfo() {
 
         <div className="Intro">This food truck sells tacos, hamburgers, pizzas etc. You can get what you've ordered in just 2 minutes!</div>
 
-        
 
+        <div className="schedule">
+          
+          <h2>Schedule</h2>
+          <CalendarIcon className="CalendarIcon w-5 h-5 text-gray-500 dark:text-gray-400" />
+          Workday: 10:00 am to 7:00 pm
+          <br/>
+          <CalendarIcon className="CalendarIcon w-5 h-5 text-gray-500 dark:text-gray-400" />
+          Weekend: 11:00 am to 7:00 pm
+        </div>
+        
 
 
       </body>
@@ -73,4 +82,27 @@ function ClockIcon(props) {
   )
 }
 
+function CalendarIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="18" height="18" x="3" y="4" rx="2" ry="2" />
+      <line x1="16" x2="16" y1="2" y2="6" />
+      <line x1="8" x2="8" y1="2" y2="6" />
+      <line x1="3" x2="21" y1="10" y2="10" />
+    </svg>
+  )
+}
+
 export default BusinessInfo;
+
