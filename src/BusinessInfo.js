422ccda140d8d7c0ea4d5313eb0a006ea0fdd47c
王烜烨 <xuanye_wang@ucsb.edu>
2024-03-03 14:06:48 -0800

    customer->business info page

--- src/BusinessInfo.js
+++ src/BusinessInfo.js
@@ -17,19 +17,19 @@ function BusinessInfo() {
       <body>
         
         <div className="location">
-          <MapPinIcon className="MapPinIcon w-5 h-5 flex-shrink-0 text-gray-500 dark:text-gray-400" />
+          <MapPinIcon className="MapPinIcon" />
           Street
         </div>
 
         <div className="open">
-          <ClockIcon className="ClockIcon w-5 h-5 flex-shrink-0 text-gray-500 dark:text-gray-400"></ClockIcon>
+          <ClockIcon className="ClockIcon"></ClockIcon>
           Open today from 10:00 am to 5:00 pm
         </div>
 
         <div className="Intro">This food truck sells tacos, hamburgers, pizzas etc. You can get what you've ordered in just 2 minutes!</div>
 
         <div className="grid gap-2">
-          <div className="stars flex items-center gap-2">
+          <div className="stars">
             <StarIcon className="filled" />
             <StarIcon className="filled" />
             <StarIcon className="filled" />
@@ -40,30 +40,32 @@ function BusinessInfo() {
         
 
         
-        <div className="schedule">
-          
-          <h2>Schedule</h2>
-          <CalendarIcon className="CalendarIcon w-5 h-5 text-gray-500 dark:text-gray-400" />
-          Workday: 10:00 am to 7:00 pm
-          <br/>
-          <CalendarIcon className="CalendarIcon w-5 h-5 text-gray-500 dark:text-gray-400" />
-          Weekend: 11:00 am to 7:00 pm
-        </div>
-        
+        <div className="horizontal-container">
+          <div className="schedule">
+            <h2>Schedule</h2>
+            <CalendarIcon className="CalendarIcon" />
+            Workday: 10:00 am to 7:00 pm
+            <br/>
+            <CalendarIcon className="CalendarIcon" />
+            Weekend: 11:00 am to 7:00 pm
+          </div>
 
-        <div className="dishes grid gap-2">
-          <h2>Featured Dishes</h2>
-          <img
-          alt="Dish #1"
-          className="rounded-lg object-cover"
-          height="160"
-          src="/placeholder.svg"
-          style={{
-          aspectRatio: "160/160",
-          objectFit: "cover",
-          }}
-          width="160"
-          />
+          <div className="dishes">
+            <h2>Featured Dishes</h2>
+            <img
+              alt="Dish #1"
+              height="260"
+              src="https://img.taste.com.au/qRDdmfsk/w720-h480-cfill-q80/taste/2022/09/healthy-tacos-recipe-181113-1.jpg"
+              //width="160"
+            />
+          </div>
+        </div>
+
+        <div className="contact">
+          Contect information:
+          <br/>
+          <br/>
+          Phone: +1 (123) 345-6789
         </div>
         
 
