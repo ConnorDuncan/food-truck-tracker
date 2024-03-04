a127507b8d3eba13d09aa0389e038b4b2123ce6b
Chris Riney <chrisriney@169-231-36-96.wireless.ucsb.edu>
2024-03-03 20:43:54 -0800

    Chris - Trying to implement a homepage frontend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
-export default () => {
-    return (
-        < >
-            <h2>Filler for website/home page</h2>
-        </>
-    )
+import React from 'react';
+import '../styling/Home.css';
+
+export default function Home() {
+  return (
+    <div className="container-style">
+      <div className="container">
+        <div className="w-1/2">
+          <h2 className="text-style">Welcome to Our Business</h2>
+          <p className="text-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.</p>
+        </div>
+        <div className="layout">
+          <button className="blue-button blue-button-with-margin">Login for Customers</button>
+          <button className="green-button green-button-with-margin">Login for Businesses</button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
