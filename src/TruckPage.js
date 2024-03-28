959001928e89d4ae4ec1d6395a60bea0debd8189
MusialaGOAT <xuanye_wang@ucsb.edu>
2024-03-28 14:52:01 -0700

    page that shows all food trucks the user has

---
+++ src/TruckPage.js
@@ -0,0 +1,52 @@
+import './TruckPage.css'
+import TruckIcon from './FoodTruckIcon'
+import CheckCircleIcon from './CheckCircleIcon'
+import ClockIcon from './ClockIcon'
+
+/*This page should appears when users login successfully using business accounts*/
+
+
+
+function BusinessPage() {
+  return (
+    <div className="Businesses">
+      <header className="Business_header">
+        <h1 className="Title">
+          Your Food Trucks
+        </h1>
+        <p className="Description">
+          Here are the trucks you own, including verified and the ones that waiting for verification
+        </p>
+      </header>
+
+      <div className="card">
+        <div className='card_content'>
+          <TruckIcon/>
+          Taco Time
+          <div class="verified">Verified</div>
+          <div className='check_circle'>
+            <CheckCircleIcon/>
+          </div>
+          <a href="/detail_page">view details</a>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className='card_content'>
+          <TruckIcon/>
+          Burger Bliss
+          <div class="waiting">Waiting for Verification</div>
+          <div className='clock_icon'>
+            <ClockIcon/>
+          </div>
+          <a href="/detail_page">view details</a>
+        </div>
+      </div>
+
+
+      
+    </div>
+  );
+}
+
+export default BusinessPage;
--- src/TruckPage.test.js
+++ src/TruckPage.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BusinessPage from './BusinessPage';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<BusinessPage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
