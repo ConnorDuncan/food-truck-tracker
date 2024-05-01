b6b5c326b7d081cbc5e22a71e414ccb336da4524
Chris Riney <chrisriney@169-231-32-164.wireless.ucsb.edu>
2024-04-30 20:45:57 -0700

    chris - trucks are now on the map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -3,22 +3,30 @@ import GoogleMap from 'google-maps-react-markers'
 import app from '../components/firebase.js';
 import { getFirestore, collection, query, getDocs } from "firebase/firestore";
 
+import 'mdui/components/navigation-rail.js';
+import 'mdui/components/navigation-rail-item.js';
+import 'mdui/components/select.js';
+import 'mdui/components/menu-item.js';
+import 'mdui/components/circular-progress.js';
+import 'mdui/components/button.js';
+
 const Map = () => {
     const [center, setCenter] = useState(null);
     const [trucks, setTrucks] = useState([]);
+    const [selected, setSelected] = useState(null);
     const db = getFirestore(app);
     let array = [];
 
-    // useEffect(() => {
-    //     const getTrucks = async () => {
-    //         const q = query(collection(db, "food-trucks"));
-    //         const trucksSnapshot = await getDocs(q);
-    //         trucksSnapshot.forEach((truck) => array.push(truck));
-    //         setTrucks(array);
-    //     } 
+    useEffect(() => {
+        const getTrucks = async () => {
+            const q = query(collection(db, "food-trucks"));
+            const trucksSnapshot = await getDocs(q);
+            trucksSnapshot.forEach((truck) => array.push(truck.data()));
+            setTrucks(array);
+        } 
         
-    //     getTrucks();
-    // })
+        getTrucks();
+    }, [])
 
     try {
         useEffect(() => {
@@ -34,51 +42,88 @@ const Map = () => {
                     navigator.geolocation.clearWatch(watchId);
                     };
             } else console.log("Geolocation is not supported by this browser.");
-        });
+        }, []);
     } catch (error) { console.log(error) }
+
+    console.log(trucks[0]);
+    console.log(center);
 
     return (
         <>
         <div style={{ display: "flex" }}>
-            <div style={{ width: "20%", padding: "10px" }}>
-                <h2>Toggle</h2>
-            </div>
-        <title>Testing</title>
-        {!center && <h1>Map is loading...</h1>}
-        {center &&
-        <GoogleMap
-            apiKey="AIzaSyCTPpsLTqqt0Dq0O-_qF6RjRE_W2CbmS_Q"
-            defaultCenter={center}
-            defaultZoom={13}
-            mapMinHeight="100vh"
-            options={{ minZoom: 17}}
-        >
-
-        {/* { trucks.map((truck) => <img
-            lat={truck['latitude']}
-            lng={truck['longitude']}
-            href='/'
-            alt={truck['business_name']}
-            src='/logo.png'
-            height='50'
-            />) }
-
-        { trucks.map((truck) => <h1>
-            lat={center['lat']}
-            lng={center['lng']}
-            testing
-            </h1>) } */}
+            <div style={{ width: "25%", padding: "2%" }}>
             
-        <img
-            lat={center['lat']}
-            lng={center['lng']}
-            href='/'
-            alt='logo'
-            src='/logo.png'
-            height='50'
-        />
-        </GoogleMap>
+            <h2>Price Range</h2>
+            <mdui-segmented-button-group selects="multiple" style={{display: 'flex', justifyContent: 'center',width: '100%'}}>
+            <mdui-segmented-button value="All">All</mdui-segmented-button>
+            <mdui-segmented-button value="$">$</mdui-segmented-button>
+            <mdui-segmented-button value="$$">$$</mdui-segmented-button>
+            <mdui-segmented-button value="$$$">$$$</mdui-segmented-button>
+            </mdui-segmented-button-group>
+            
+            <h2 style = {{marginTop: '100px'}}>Food Types</h2>
+            <mdui-select multiple style = {{width: '100%'}}>
+                <mdui-menu-item value="Mexican">Mexican</mdui-menu-item>
+                <mdui-menu-item value="Chinese">Chinese</mdui-menu-item>
+                <mdui-menu-item value="American">American</mdui-menu-item>
+                <mdui-menu-item value="Taco">Taco</mdui-menu-item>
+                <mdui-menu-item value="Hamburger">Hamburger</mdui-menu-item>
+                <mdui-menu-item value="Pizza">Pizza</mdui-menu-item>
+            </mdui-select>
+
+            <mdui-button variant="tonal" style = {{marginTop: '100px', width: '100%', display: 'flex', justifyContent: 'center'}}>Save Preferences</mdui-button>
+
+            </div>
+        
+        {!center && (
+        <>  
+            <div style={{position: 'absolute', top: '50%', left: '60%', transform: 'translate(-50%, -50%)'}}>
+                <h1>Just a moment...</h1>
+                <mdui-circular-progress style={{left: '35%'}}></mdui-circular-progress>
+            </div>
+        </>
+        )}
+
+        {center &&
+        <div style={{ width: "100%", overflow: "hidden", justifyContent: 'center', display: 'flex', alignItems: 'center'}}>
+            <GoogleMap
+                apiKey="AIzaSyCTPpsLTqqt0Dq0O-_qF6RjRE_W2CbmS_Q"
+                defaultCenter={center}
+                defaultZoom={13}
+                mapMinHeight="100vh"
+                // options={{ minZoom: 17}}
+            >
+
+            { trucks && trucks.map((truck) => 
+                truck['open'] &&
+                truck['verified'] &&
+                <img
+                    style={ {borderRadius: '100%', justifyContent: 'center', display: 'flex', alignItems: 'center'} }
+                    lat={truck['location']._lat}
+                    lng={truck['location']._long}
+                    href='/'
+                    alt={truck['business_name']}
+                    src={truck['logo']}
+                    height='50'
+                    onClick={() => setSelected({'item': 'test'})}
+                />) }
+                <img
+                    style={{ borderRadius: '100%', justifyContent: 'center', display: 'flex', alignItems: 'center' }}
+                    lat={center['lat']}
+                    lng={center['lng']}
+                    href='/'
+                    alt='logo'
+                    src='/logo.png'
+                    height='50'
+                />
+
+            {selected && 
+                <h1 onClick={() => setSelected(null)}>
+                    A truck has been selected
+                </h1>
         }
+            </GoogleMap>
+        </div> }
         </div>
         </>
     )
